90d4d3a4275181bc219335dde5c2af6b829d9c4f
doctor <fd5@MacBook-Air-2022.local>
2023-11-29 14:35:32 +0700

    hack hour

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,12 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+  add: (a, b) => a + b,
+  minus: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
+let addResult = calculator.add;
+let divideResult = calculator.divide;
+
+console.log(addResult(2, 4));
+console.log(divideResult(3000, 10));
