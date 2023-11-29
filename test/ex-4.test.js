a019679ef3cc87f95427cd45073613c215a5c0bd
lacitadelle <jkomala99@gmail.com>
2023-11-29 13:18:10 +0700

    fixed even more division typo

--- test/ex-4.test.js
+++ test/ex-4.test.js
@@ -3,9 +3,9 @@ import fs from "fs/promises";
 describe("exercise-4 : test JS Function Hack Hour", () => {
   test("Function ต้องทำงานและได้ผลลัพธ์ตามที่โจทย์กำหนด", async () => {
     const data = await fs.readFile("./ex-4.js");
-    const code = `${data} \n return {calculator, addResult, devideResult}`;
+    const code = `${data} \n return {calculator, addResult, divideResult}`;
     const func = new Function(code);
-    const { calculator, addResult, devideResult } = func();
+    const { calculator, addResult, divideResult } = func();
     expect(calculator.add(10, 2)).toBe(12);
     expect(calculator.subtract(10, 2)).toBe(8);
     expect(calculator.multiply(10, 2)).toBe(20);
