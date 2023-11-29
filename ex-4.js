25ad71453ce4aa85e9b83de8a369bd06d6d1cfb5
kaweeracha <kaweeracha@MacBook-Air-khxng-kaweeracha.local>
2023-11-29 15:06:14 +0700

    ex1:2:3:4:5:6

--- ex-4.js
+++ ex-4.js
@@ -1,2 +1,13 @@
 // Start coding here
-let calculator = {};
+let calculator = {
+    add: (a, b ) => a + b,
+    minus: (a, b) => a - b,
+    muliplay: (a, b) => a * b,
+    divide: (a, b) => a / b,
+};
+
+let addResult = calculator.add;
+console.log(addResult(10, 20))
+let divideResult = calculator.divide
+console.log(divideResult(3000, 10))
+
